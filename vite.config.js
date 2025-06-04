e28ebfa28b59be3049e379950c0816e60bb37809
waseeqansari-soc <waseeq.ansari@soc-solution.com>
2025-06-04 21:05:27 +0500

    Fix/Allowed Production host

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['admin.takeoffyachts.com']
+  }
 });
